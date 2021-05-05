cf867b5153958f2791a163e5c5258f0f7dbe7b9a
elsanfernandes <elson.cool@gmail.com>
2021-05-05 12:30:07 +0530

    Initial commit

---
+++ src/test/java/com/rummer/config/SoaCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rummer.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoaCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
